efc5071b04344770e775eba03a49a218ddf0b293
Orie McIntosh <dev@eighttwelveconsulting.com>
2024-06-07 08:50:44 -0400

    Initial Commit

---
+++ AwsAmplifyApp/AwsAmplifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwsAmplifyAppApp.swift
+//  AwsAmplifyApp
+//
+//  Created by Orie McIntosh on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AwsAmplifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
